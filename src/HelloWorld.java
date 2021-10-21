45dcea7648e83783b1b6dafc3a38ad03bbe37623
john alejandro <john.e.alejandro01@gmail.com>
2021-10-21 16:13:57 -0500

    first push on java

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {            // Will not run if main method is not used
+
+                System.out.print("Hello,");
+                System.out.print(" World!");     // Creates new line
+
+    }
+
+}
