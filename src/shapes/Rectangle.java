78d47d6b36adf32170b123afdc093bd08ca975c7
john alejandro <john.e.alejandro01@gmail.com>
2021-11-04 00:18:56 -0500

    Completed after review

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,32 @@
+package shapes;
+
+import org.w3c.dom.css.Rect;
+
+public class Rectangle {
+//    Shapes
+//
+//    Inside of your shapes directory, create a class named Rectangle. It should have protected properties for both length and width. Rectangle should define a constructor that accepts two numbers for length and width, and sets those properties.
+//
+//    Create two methods on the Rectangle class, getArea and getPerimeter that return the respective values. The formulas for both follow:
+//
+//
+//    perimeter = 2 x length + 2 x width
+//    area = length x width
+
+    protected double length;
+    protected double width;
+
+    //constructor
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea() {
+        return this.length * this.width;
+    }
+
+    public double getPerimeter() {
+        return 2 * (this.length + this.width);
+    }
+}
